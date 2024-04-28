b68f2f8a398646d354f976536039467ebc5d4da0
Anas-Aqrabawi <aq-50@proton.me>
2024-04-29 00:22:13 +0300

    Add project files.

---
+++ recipes_webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace recipes_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
